a107ccf34515f782318756f2d31d311c646014fd
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2024-06-05 17:19:44 +0700

    comment IsNumber

--- src/order/dto/get.order.list.dto.ts
+++ src/order/dto/get.order.list.dto.ts
@@ -1,14 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class GetOrderListDTO {
-  @ApiProperty({ example: 0, type: Number })
-  @IsNotEmpty()
-  @IsNumber()
-  take: number;
-
   @ApiProperty({ example: 10, type: Number })
   @IsNotEmpty()
-  @IsNumber()
+  // @IsNumber()
+  take: number;
+
+  @ApiProperty({ example: 0, type: Number })
+  @IsNotEmpty()
+  // @IsNumber()
   skip: number;
 }
